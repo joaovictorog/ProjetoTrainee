f6b836a6388f31740e40803ac2f9f3bcd8b359de
Luiz Eduardo <164056390+Dadooa@users.noreply.github.com>
2025-01-17 15:34:21 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -34,23 +34,3 @@ async function main() {
 }
 
 main()*/
-
-import MusicaService from "./src/domains/Musica/MusicaService";
-
-async function main() {
-    const body = {
-        ID_Musica: 2,
-        Nome: "Naquela Mesa",
-        ArtistaID: 3,
-        Genero: "MPB",
-        AlbumID: 4,
-        Num_Streams: 36805046,
-        Data_Lancamento: new Date("1974-01-01"),
-    }
-    
-    const usuario = await MusicaService.create(body)
-
-    console.log(usuario)
-}
-
-main()
