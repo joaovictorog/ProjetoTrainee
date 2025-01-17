721071ee4379f438643841ee11a3a1a40b9b61b5
João Victor Otoni <jvog09@gmail.com>
2025-01-16 21:11:21 -0300

    feat: Addition of the Artista table create function

--- src/domains/Artista/ArtistaService.ts
+++ src/domains/Artista/ArtistaService.ts
@@ -1,6 +1,18 @@
 import { Artista } from "@prisma/client";
 import prisma from "../../../config/prismaClient";
 
-class ArtistaService {}
+class ArtistaService {
+    async create(body: Artista) {
+        const Artista = await prisma.artista.create({
+            data: {
+                Nome: body.Nome,
+                Foto: body.Foto,
+                Num_Streams: body.Num_Streams
+            }
+        });
+
+        return Artista;
+    }
+}
 
 export default new ArtistaService();
