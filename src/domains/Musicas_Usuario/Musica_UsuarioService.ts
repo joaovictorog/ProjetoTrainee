744ad27a672015f0bb8621e4083e4d089cdaa024
Luiz Eduardo <dadoarantes2@gmail.com.br>
2025-01-19 07:37:10 -0300

    feat: create da entidade Musicas_Usuario criado

--- src/domains/Musicas_Usuario/Musica_UsuarioService.ts
+++ src/domains/Musicas_Usuario/Musica_UsuarioService.ts
@@ -1,2 +1,15 @@
 import { Musicas_Usuario } from "@prisma/client";
 import prisma from "../../../config/prismaClient";
+
+class MusicaUsuarioService {
+    async create(body: Musicas_Usuario) {
+        const musicaUsuario = await prisma.musicas_Usuario.create({
+            data: {
+                UsuarioID: body.UsuarioID,
+                ID_Musica: body.ID_Musica,
+                Data_Criacao: body.Data_Criacao,
+            },
+        });
+        return musicaUsuario;
+    }
+}
