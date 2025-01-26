9a5df176ab76bf02c4035d64acd016b8df7bb4de
Dadooa <dadoarantes2@gmail.com>
2025-01-26 17:57:07 -0300

    adicionei o MusicaService dentor da pasta Service

--- src/domains/Musica/services/MusicaService.ts
+++ src/domains/Musica/services/MusicaService.ts
@@ -1,5 +1,5 @@
 import { Musica } from "@prisma/client";
-import prisma from "../../../config/prismaClient";
+import prisma from "../../../../config/prismaClient";
 
 class MusicaService {
     async create(body: Musica){
