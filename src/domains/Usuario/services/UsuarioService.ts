ad43d63b1fa00e06bd45a1221ffaeaf4bc32e572
João Victor Otoni <jvog09@gmail.com>
2025-01-26 16:19:22 -0300

    feat: Moving UsuarioService to services folder

--- src/domains/Usuario/services/UsuarioService.ts
+++ src/domains/Usuario/services/UsuarioService.ts
@@ -1,5 +1,5 @@
 import { Usuario } from "@prisma/client";
-import prisma from "../../../config/prismaClient";
+import prisma from "../../../../config/prismaClient";
 
 class UsuarioService {
     async create(body: Usuario){
