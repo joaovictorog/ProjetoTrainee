143ce075dc70317d2fa0800a2c7c1f00da8ba6dd
Tomas <tomasbhmg@gmail.com>
2025-01-27 20:39:26 -0300

    adiciona função deletar album

--- src/domains/Album/controllers/albumController.ts
+++ src/domains/Album/controllers/albumController.ts
@@ -13,27 +13,36 @@ router.get("/", async (req:Request, res:Response, next:NextFunction) => {
 });
 
 router.get("/:id", async (req:Request, res:Response, next:NextFunction) => {
-   try {
+    try {
         const album = await AlbumService.findById(Number(req.params.id))
         res.json(album);
-   } catch (error) {
+    } catch (error) {
         next(error);
-   } 
+    } 
 });
 
 router.post("/", async (req:Request, res:Response, next:NextFunction) => {
-   try {
-    const novoAlbum = await AlbumService.create(req.body)
-    res.status(201).json(novoAlbum)
-   } catch (error) {
-    next(error)
-   } 
+    try {
+        const novoAlbum = await AlbumService.create(req.body)
+        res.status(201).json(novoAlbum)
+    } catch (error) {
+        next(error)
+    } 
 });
 
 router.put("/:id", async (req:Request, res:Response, next:NextFunction) => {
     try {
         const updatedAlbum = await AlbumService.update(Number(req.params.id), req.body)
         res.json(updatedAlbum)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.delete("/:id", async (req:Request, res:Response, next:NextFunction) => {
+    try {
+        const deletedAlbum = await AlbumService.delete(Number(req.params.id))
+        res.json(deletedAlbum)
     } catch (error) {
         next(error)
     }
