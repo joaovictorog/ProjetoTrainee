73efd0198a508863cad7a01c7888de5c032c828e
Tomas <tomasbhmg@gmail.com>
2025-01-27 21:05:26 -0300

    reconfig expressconfig

--- config/expressConfig.ts
+++ config/expressConfig.ts
@@ -1,10 +1,12 @@
 import cors, { CorsOptions } from "cors";
 import dotenv from "dotenv";
 import express, { Express } from "express";
+
 import UsuarioRouter from "../src/domains/Usuario/controllers/UsuarioController";
 import ArtistaRouter from "../src/domains/Artista/controllers/ArtistaController";
-import MusicaRouter from "../src/domains/Musica/controllers/MusicaController"
+import MusicaRouter from "../src/domains/Musica/controllers/MusicaController";
 import MusicaUsuarioRouter from "../src/domains/Musicas_Usuario/controllers/Musica_UsuarioController";
+import AlbumRouter from "../src/domains/Album/controllers/albumController";
 
 export const app: Express = express();
 
@@ -13,13 +15,15 @@ dotenv.config();
 const options: CorsOptions = {
     credentials: true,
     origin: process.env.APP_URL
-}
+};
 
 app.use(cors(options));
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+
+app.use("/api/albuns", AlbumRouter);
 app.use("/api/usuarios", UsuarioRouter);
 app.use("/api/artistas", ArtistaRouter);
 app.use("/api/musicas", MusicaRouter);
