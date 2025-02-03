2a6a6fabacd5f9504cf794a9008afed889ff3c3e
João Victor Otoni <jvog09@gmail.com>
2025-02-02 21:53:06 -0300

    feat: Adding statusCodes to responses

--- src/domains/Artista/controllers/ArtistaController.ts
+++ src/domains/Artista/controllers/ArtistaController.ts
@@ -1,12 +1,13 @@
 import { Router, Request, Response, NextFunction } from "express";
 import ArtistaService from "../services/ArtistaService";
+import statusCodes from "../../../../utils/constants/statusCodes";
 
 const router = Router();
 
 router.post("/", async (req: Request, res: Response, next: NextFunction) => {
     try {
         const novoArtista = await ArtistaService.create(req.body);
-        res.status(201).json(novoArtista);
+        res.status(statusCodes.SUCCESS).json(novoArtista);
     } catch (error) {
         next(error);
     }
@@ -15,7 +16,7 @@ router.post("/", async (req: Request, res: Response, next: NextFunction) => {
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
     try {
         const artistas = await ArtistaService.findAll();
-        res.json(artistas);
+        res.status(statusCodes.SUCCESS).json(artistas);
     } catch (error) {
         next(error);
     }
@@ -24,7 +25,7 @@ router.get("/", async (req: Request, res: Response, next: NextFunction) => {
 router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
     try {
         const artista = await ArtistaService.findById(Number(req.params.id));
-        res.json(artista);
+        res.status(statusCodes.SUCCESS).json(artista);
     } catch (error) {
         next(error);
     }
@@ -33,7 +34,7 @@ router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
 router.put("/:id", async (req: Request, res: Response, next: NextFunction) => {
     try {
         const artistaAtualizado = await ArtistaService.update(Number(req.params.id), req.body);
-        res.json(artistaAtualizado);
+        res.status(statusCodes.SUCCESS).json(artistaAtualizado);
     } catch (error) {
         next(error);
     }
@@ -42,7 +43,7 @@ router.put("/:id", async (req: Request, res: Response, next: NextFunction) => {
 router.delete("/:id", async (req: Request, res: Response, next: NextFunction) => {
     try {
         const artistaDeletado = await ArtistaService.delete(Number(req.params.id));
-        res.json(artistaDeletado);
+        res.status(statusCodes.SUCCESS).json(artistaDeletado);
     } catch (error) {
         next(error);
     }
