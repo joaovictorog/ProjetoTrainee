ada71ef552ff5491590cb48a8909cddebc82a1a6
João Victor Otoni <jvog09@gmail.com>
2025-02-16 20:54:17 -0300

    feat: Create user tests

--- src/domains/Usuario/services/UsuarioService.test.ts
+++ src/domains/Usuario/services/UsuarioService.test.ts
@@ -3,3 +3,32 @@ import { InvalidParamError } from "../../../../errors/InvalidParamError";
 import { QueryError } from "../../../../errors/QueryError";
 import { prismaMock } from "../../../../config/singleton";
 import { mockReset } from "jest-mock-extended";
+
+describe("UsuarioService", () => {
+    beforeEach(() => {
+        mockReset(prismaMock);
+    });
+    
+    describe("create", () => {
+        test("recebe dados válidos de usuário ==> cria um novo usuário", async () => {
+            const usuario = { ID_Usuario: 1, Email: "teste@.com", Nome: "usuario", Senha: "encrypted", isAdmin: false, Foto: "jpg" };
+
+            prismaMock.usuario.create.mockResolvedValue(usuario);
+
+            await expect(UsuarioService.create(usuario)).resolves.toEqual(usuario);
+        });
+
+        test("tenta criar usúario sem email ==> lança InvalidParamError", async () => {
+            const invalidUser = { ID_Usuario: 1, Email: null, Nome: "usuario", Senha: "encrypted", isAdmin: false, Foto: "jpg" };
+
+            await expect(UsuarioService.create(invalidUser as any)).rejects.toThrow(InvalidParamError);
+        });
+
+        test("tenta criar usúario sem nome ==> lança InvalidParamError", async () => {
+            const invalidUser = { ID_Usuario: 1, Email: "teste@.com", Nome: null, Senha: "encrypted", isAdmin: false, Foto: "jpg" };
+
+            await expect(UsuarioService.create(invalidUser as any)).rejects.toThrow(InvalidParamError);
+        });        
+    });
+
+});
