680d098db7af28af067c72d3589e069fbb4a26a7
João Victor Otoni <jvog09@gmail.com>
2025-02-16 20:51:10 -0300

    feat: Creating usuario test file and importing functions

---
+++ src/domains/Usuario/services/UsuarioService.test.ts
@@ -0,0 +1,5 @@
+import UsuarioService from "././UsuarioService";
+import { InvalidParamError } from "../../../../errors/InvalidParamError"; 
+import { QueryError } from "../../../../errors/QueryError";
+import { prismaMock } from "../../../../config/singleton";
+import { mockReset } from "jest-mock-extended";
