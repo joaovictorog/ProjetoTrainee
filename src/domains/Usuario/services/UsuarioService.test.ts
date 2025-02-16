b579454e041d7a663149f172ede637268b802779
João Victor Otoni <jvog09@gmail.com>
2025-02-16 20:55:29 -0300

    feat: Read user tests

--- src/domains/Usuario/services/UsuarioService.test.ts
+++ src/domains/Usuario/services/UsuarioService.test.ts
@@ -31,4 +31,61 @@ describe("UsuarioService", () => {
         });        
     });
 
+    describe("findAll", () => {
+        test("usuários existentes ==> retorna todos os usuários", async () => {
+            const usuario = [{ ID_Usuario: 1, Email: "teste@.com", Nome: "usuario", Senha: "encrypted", isAdmin: false, Foto: "jpg" }];
+
+            prismaMock.usuario.findMany.mockResolvedValue(usuario);
+
+            await expect(UsuarioService.findAll()).resolves.toEqual(usuario);
+        });
+
+        test("usuários inexistentes ==> lança QueryError", async () => {
+            prismaMock.usuario.findMany.mockResolvedValue([]);
+
+            await expect(UsuarioService.findAll()).rejects.toThrow(QueryError);
+        });
+    });
+
+    describe("findByEmail", () => {
+        test("email válido fornecido ==> retorna usuário correspondente", async () => {
+            const usuario = { ID_Usuario: 1, Email: "teste@.com", Nome: "usuario", Senha: "encrypted", isAdmin: false, Foto: "jpg" };
+
+            prismaMock.usuario.findUnique.mockResolvedValue(usuario);
+
+            await expect(UsuarioService.findByEmail("teste@.com")).resolves.toEqual(usuario);
+        });
+
+        test("usuário não encontrado por email ==> lança QueryError", async () => {
+            prismaMock.usuario.findUnique.mockResolvedValue(null);
+
+            await expect(UsuarioService.findByEmail("teste@.com")).rejects.toThrow(QueryError);
+        });
+
+        test("email não fornecido ==> lança InvalidParamError", async () => {
+            await expect(UsuarioService.findByEmail(null as any)).rejects.toThrow(InvalidParamError);
+        });
+
+    });
+
+    describe("findById", () => {
+        test("ID válido fornecido ==> retorna usuário correspondente", async () => {
+            const usuario = { ID_Usuario: 1, Email: "teste@.com", Nome: "usuario", Senha: "encrypted", isAdmin: false, Foto: "jpg" };
+
+            prismaMock.usuario.findUnique.mockResolvedValue(usuario);
+
+            await expect(UsuarioService.findById(1)).resolves.toEqual(usuario);
+        });
+
+        test("usuário não encontrado por ID ==> lança QueryError", async () => {
+            prismaMock.usuario.findUnique.mockResolvedValue(null);
+
+            await expect(UsuarioService.findById(1)).rejects.toThrow(QueryError);
+        });
+
+        test("ID não fornecido ==> lança InvalidParamError", async () => {
+            await expect(UsuarioService.findById(null as any)).rejects.toThrow(InvalidParamError);
+        });
+    });
+
 });
