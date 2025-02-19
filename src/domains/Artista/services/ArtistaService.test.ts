fdb1f1ffd8df9db6580094b49db50cafba8a062f
Tomas <tomasbhmg@gmail.com>
2025-02-19 09:57:30 -0300

    feat: created test file for artistaservice

---
+++ src/domains/Artista/services/ArtistaService.test.ts
@@ -0,0 +1,14 @@
+import ArtistaService from "./ArtistaService";
+import { prismaMock } from "../../../../config/singleton";
+import { mockReset } from "jest-mock-extended";
+
+describe("ArtistaService", () => {
+    beforeEach(() => {
+        mockReset(prismaMock);
+    });
+
+    describe("create", async () => {
+        
+    })
+
+});
