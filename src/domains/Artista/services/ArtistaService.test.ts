12a6ee37924fbf3e8da15cf891dbcf5b5fcc0f4f
Tomas <tomasbhmg@gmail.com>
2025-02-19 10:10:43 -0300

    feat: função para testar o metodo create e findall

--- src/domains/Artista/services/ArtistaService.test.ts
+++ src/domains/Artista/services/ArtistaService.test.ts
@@ -7,8 +7,32 @@ describe("ArtistaService", () => {
         mockReset(prismaMock);
     });
 
-    describe("create", async () => {
-        
-    })
+    describe("create", () => {
+        test("dados válidos ==> cria um novo artista", async () => {
+            const artista = {
+                ID_Artista: 1,
+                Nome: "Artista",
+                Foto: null,
+                Num_Streams: 200
+            };
+            prismaMock.artista.create.mockResolvedValue(artista);
+
+            await expect(ArtistaService.create(artista)).resolves.toEqual(artista);
+        });
+    });
+    describe("findAll", () => {
+        test("dados válidos ==> retorna todos os artistas", async () => {
+            const artista = [{
+                ID_Artista: 1,
+                Nome: "Artista",
+                Foto: null,
+                Num_Streams: 200
+            }];
+            
+            prismaMock.artista.findMany.mockResolvedValue(artista);
+            await expect(ArtistaService.findAll()).resolves.toEqual(artista);
+        });
+    });
+    describe("")
 
 });
